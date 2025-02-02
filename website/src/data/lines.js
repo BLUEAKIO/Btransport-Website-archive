ab4c084558942c7c2720525bad6580887186798e
NiuBoss123 <niuboss123@foxmail.com>
2025-02-02 19:14:34 +0800

    修改语言标签

--- website/src/data/lines.js
+++ website/src/data/lines.js
@@ -2,8 +2,8 @@ export const companies = [
   {
     id: 'TradeWind-Subway',
     name: {
-      'zh-Hans': '信风地铁',
-      'zh-Hant': '信風地鐵',
+      'zh-CN': '信风地铁',
+      'zh-HK': '信風地鐵',
       'en': 'TradeWind Subway',
     },
     lines: [],
@@ -11,50 +11,50 @@ export const companies = [
   {
     id: 'Hoshino-Railway',
     name: {
-      'zh-Hans': '星野铁道',
-      'zh-Hant': '星野鐵道',
+      'zh-CN': '星野铁道',
+      'zh-HK': '星野鐵道',
       'en': 'Hoshino Railway',
     },
     lines: [
       {
         id: 'HH',
         name: {
-          'zh-Hans': '星野本线',
-          'zh-Hant': '星野铁道',
+          'zh-CN': '星野本线',
+          'zh-HK': '星野铁道',
           'en': 'Hoshino Main Line',
         },
         operationalStatus: 'partialSuspension',
         operationalInfo: {
-          'zh-Hans': '因施工影响，阿拜多斯站至涅普缇努站区间暂停服务',
-          'zh-Hant': '因施工影響，阿拜多斯站至涅普緹努站區間暫停服務',
+          'zh-CN': '因施工影响，阿拜多斯站至涅普缇努站区间暂停服务',
+          'zh-HK': '因施工影響，阿拜多斯站至涅普緹努站區間暫停服務',
           'en': 'Due to construction, service between Abydos and Neptuno is suspended',
         },
       },
       {
         id: 'HS',
         name: {
-          'zh-Hans': '白子线',
-          'zh-Hant': '白子綫',
+          'zh-CN': '白子线',
+          'zh-HK': '白子綫',
           'en': 'Shiroko Line',
         },
         operationalStatus: 'normal',
         operationalInfo: {
-          'zh-Hans': '正常运营',
-          'zh-Hant': '正常運營',
+          'zh-CN': '正常运营',
+          'zh-HK': '正常運營',
           'en': 'Operating normally',
         },
       },
       {
         id: 'HK',
         name: {
-          'zh-Hans': '栀子线',
-          'zh-Hant': '梔子線',
+          'zh-CN': '栀子线',
+          'zh-HK': '梔子線',
           'en': 'Kuchinashi Line',
         },
         operationalStatus: 'suspended',
         operationalInfo: {
-          'zh-Hans': '栀子线全线修建中',
-          'zh-Hant': '梔子線全線興建中',
+          'zh-CN': '栀子线全线修建中',
+          'zh-HK': '梔子線全線興建中',
           'en': 'Kuchinashi Line under construction in its entirety',
         },
       },
--- website/src/i18n.js
+++ website/src/i18n.js
@@ -2,25 +2,25 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import enTranslation from './i18n/en.json';
-import zhHansTranslation from './i18n/zh-Hans.json';
-import zhHantTranslation from './i18n/zh-Hant.json';
+import zhCNTranslation from './i18n/zh-CN.json';
+import zhHKTranslation from './i18n/zh-HK.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
+      'zh-CN': {
+        translation: zhCNTranslation,
+      },
+      'zh-HK': {
+        translation: zhHKTranslation,
+      },
       en: {
         translation: enTranslation,
       },
-      'zh-Hans': {
-        translation: zhHansTranslation,
-      },
-      'zh-Hant': {
-        translation: zhHantTranslation,
-      },
     },
-    fallbackLng: 'zh-Hans',
+    fallbackLng: 'zh-CN',
     debug: true,
     interpolation: {
       escapeValue: false,
@@ -28,39 +28,7 @@ i18n
     detection: {
       order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
       caches: ['cookie', 'localStorage'],
-      lookupQuerystring: 'lng',
-      lookupCookie: 'i18next',
-      lookupLocalStorage: 'i18nextLng',
-      checkWhitelist: true,
-      cookieMinutes: 10,
-      cookieDomain: 'myDomain',
-      htmlTag: document.documentElement,
-      cookieOptions: { path: '/', sameSite: 'strict' }
-    },
-    whitelist: ['en', 'zh-Hans', 'zh-Hant'],
-    load: 'languageOnly',
-    parseMissingKeyHandler: (key) => {
-      if (key.startsWith('zh')) {
-        if (key === 'zh-CN' || key === 'zh-SG') {
-          return 'zh-Hans';
-        } else if (key === 'zh-HK' || key === 'zh-TW' || key === 'zh-MO') {
-          return 'zh-Hant';
-        }
-      }
-      return key;
     },
   });
-
-i18n.on('languageChanged', (lng) => {
-  const options = i18n.options.detection;
-  if (options.caches) {
-    if (options.caches.includes('cookie')) {
-      document.cookie = `i18next=${lng};path=/;domain=${options.cookieDomain};max-age=${options.cookieMinutes * 60}`;
-    }
-    if (options.caches.includes('localStorage')) {
-      localStorage.setItem('i18nextLng', lng);
-    }
-  }
-});
 
 export default i18n;
